6db517d10db828abc035ba4b6c1e25009de1b68c
raquelsilva2006 <raquelsilva2006@cdd5694b-b841-0410-a4ec-4f9d8101e2a1>
2007-12-21 20:48:40 +0000

    Dominio de todos os projetos!!!!!!!!!!!!!!1 SE LIGUEM!! :P

---
+++ DominioEVendas/src/dominio/Caixa.java
@@ -0,0 +1,7 @@
+package dominio;
+
+//quando instanciar o funcionario pra nao dizer que naa venda eh uma
+//classe caixa...
+public class Caixa extends Funcionario{
+
+}
---
+++ DominioEVendas/src/dominio/Cliente.java
@@ -0,0 +1,32 @@
+package dominio;
+
+public class Cliente {
+	
+	private String id;
+	private String nome;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Cliente(String id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
