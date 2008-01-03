a5d688534ac47b7bbf69a97bbbaec81eba75c124
wkelyson@gmail.com <wkelyson@gmail.com@cdd5694b-b841-0410-a4ec-4f9d8101e2a1>
2008-01-03 22:45:13 +0000

    Removed file/folder

--- trunk/PDV-core/src/br/cefetrn/datinf/credito/remoto/ICredito.java
+++
@@ -1,6 +0,0 @@
-package br.cefetrn.datinf.credito.remoto;
-
-public interface ICredito {
-public String solicitarAprovacaoDeCompra(String numeroCartao,double valorCompra,
-    		int qtdParcelas,String identPDV);
-}
