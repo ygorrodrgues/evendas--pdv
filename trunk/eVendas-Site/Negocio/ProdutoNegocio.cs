1fcc51d6e4cf34e94038540c0bccf2beb28ace93
r4faelmf <r4faelmf@cdd5694b-b841-0410-a4ec-4f9d8101e2a1>
2008-01-07 04:00:59 +0000

    Added a folder remotely

---
+++ trunk/eVendas-Site/Negocio/ProdutoNegocio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using DAL;
+
+namespace Negocio
+{
+    public class ProdutoNegocio
+    {
+        public DataSet Select()
+        {
+            ProdutoDAL dal = new ProdutoDAL();
+            return dal.Select();
+        }
+    }
+}
