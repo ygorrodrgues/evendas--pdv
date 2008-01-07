f513db66c838f52560d3cee9d5d97d30e3d434ce
r4faelmf <r4faelmf@cdd5694b-b841-0410-a4ec-4f9d8101e2a1>
2008-01-07 04:00:59 +0000

    Added a folder remotely

---
+++ eVendas-Site/Negocio/ClienteNegocio.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using DAL;
+using Dominio;
+
+namespace Negocio
+{
+    public class ClienteNegocio
+    {
+        public DataSet SelectEndereco(Usuario usuario)
+        {
+            ClienteDAL dal = new ClienteDAL();
+            return dal.SelectEndereco(usuario);
+        }
+    }
+}
